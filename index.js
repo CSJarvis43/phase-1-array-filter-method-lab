89a5ed434c06589806e646d0cefbe9d17d99a6c0
Charles Jarvis <csjarvis43@gmail.com>
2022-06-11 12:23:43 -0600

    commit tree a1981932a4cce4c58a5b904f50dc04fcc07e2a88

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+// function findMatching(array, driver){
+//     return array.filter(el => el.toLowerCase() === driver.toLowerCase())
+
+// }
+
+// function fuzzyMatch(array, driver){
+//     return array.filter(el => el.toLowerCase() === driver.includes())
+// }
+
+// const partialDrivers = drivers.filter(matchDriver)
+
+// function matchDriver(driver){
+//   if(driver === drivers.name){
+//     console.log(driver)
+//     return drivers.name
+//   }
+// }
+// matchDriver('Bobby')
+
+function matchDriver(array, string) {
+  let matches = array.filter(function(str) {
+    return str.includes(string) === string
+  })
+  return (matches.length > 0) ? matches: string
+}
+
+console.log(matchDriver(drivers, "b"))
